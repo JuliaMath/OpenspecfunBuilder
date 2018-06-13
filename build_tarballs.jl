5572e10532c4b8c74c38266b1fee5726b2c72f05
Steven G. Johnson <stevenj@mit.edu>
2018-06-13 17:18:55 -0400

    update build script
    For some reason the build script was listing the `LibraryProduct` three times.  It was also calling `autobuild` rather than `build_tarballs`, and manually doing a bunch of the things that `build_tarballs` does for you — was this from a really old version of the wizard?
    
    It also might be nicer to use `platforms = supported_platforms()` and then `filter!` out any platforms that don't work (FreeBSD, I guess?).

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,7 +2,7 @@ using BinaryBuilder
 
 # These are the platforms built inside the wizard
 platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
+  BinaryProvider.Linux(:i686, :glibc),
   BinaryProvider.Linux(:x86_64, :glibc),
   BinaryProvider.Linux(:aarch64, :glibc),
   BinaryProvider.Linux(:armv7l, :glibc),
@@ -12,14 +12,6 @@ platforms = [
   BinaryProvider.Windows(:x86_64)
 ]
 
-
-# If the user passed in a platform (or a few, comma-separated) on the
-# command-line, use that instead of our default platforms
-if length(ARGS) > 0
-    platforms = platform_key.(split(ARGS[1], ","))
-end
-info("Building for $(join(triplet.(platforms), ", "))")
-
 # Collection of sources required to build openspecfun
 sources = [
     "https://github.com/JuliaLang/openspecfun/archive/v0.5.3.tar.gz" =>
@@ -27,26 +19,15 @@ sources = [
 ]
 
 script = raw"""
-cd $WORKSPACE/srcdir
-cd openspecfun-0.5.3/
+cd $WORKSPACE/srcdir/openspecfun-0.5.3
 make
-mkdir ../../destdir/lib/
-cp libopenspecfun.* ../../destdir/lib/
-
+mkpath $prefix/lib
+cp libopenspecfun.* $prefix/lib
 """
 
-products = prefix -> [
-    LibraryProduct(prefix,"libopenspecfun"),
-    LibraryProduct(prefix,"libopenspecfun"),
+products(prefix) = [
     LibraryProduct(prefix,"libopenspecfun")
 ]
 
-
 # Build the given platforms using the given sources
-hashes = autobuild(pwd(), "openspecfun", platforms, sources, script, products)
-
-if !isempty(get(ENV,"TRAVIS_TAG",""))
-    print_buildjl(pwd(), products, hashes,
-        "https://github.com/KristofferC/OpenspecfunBuilder/releases/download/$(ENV["TRAVIS_TAG"])")
-end
-
+hashes = build_tarballs(ARGS, "openspecfun", sources, script, platforms, products, [])
